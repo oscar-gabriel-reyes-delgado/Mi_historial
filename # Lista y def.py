096d3add9ae72540e8978a76e66112a4ba02a8f0
Oscar Reyes <AL07163611@tecmilenio.mx>
2025-09-03 14:18:57 -0600

    Notas de la actividad

--- # Lista y def.py
+++ # Lista y def.py
@@ -1,15 +1,19 @@
 # Lista y def
+
+# Notas: El lista.sort()
+# Se deja vacio el parentesis para que te acomode los numeros de menor a mayor
+# Se pone reverse=True dentro del parentesis para que se acomoden de mayor a menor
+
+
 numeros = [1,3, 45, 65, 567, 45, 34]
 def lista_creciente(numeros):
     numeros.sort()
-    # Esto es para imprimir una lista ya definida de puros numeros de mayor a menor
     print(numeros)
 
 lista_creciente(numeros)
 
 def lista_decreciente(numeros):
     numeros.sort(reverse = True)
-    # Esto es para imprimir una lista ya definida de puros numeros de menor a mayor
     print(numeros)
 
 lista_decreciente(numeros)
